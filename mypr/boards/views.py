1f8edb0f06f53a6c99d491226d8d8da79545b30f
Preeti-Pundir <preetipundir340@gmail.com>
2021-08-16 13:57:05 +0530

    First commit

---
+++ mypr/boards/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
+'''from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello, preeti welcome back!')'''
+
+from django.http import HttpResponse
+from .models import Board
+from django.http import Http404
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
+
+'''def board_topics(request, pk):
+        board = Board.objects.get(pk=pk)
+        return render(request, 'topics.html', {'board': board})'''
+
+
+
+
+
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+        board = get_object_or_404(Board, pk=pk)
+        return render(request, 'new_topic.html', {'board': board})
+ 
